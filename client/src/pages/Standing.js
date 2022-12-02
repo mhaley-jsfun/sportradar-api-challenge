4d74393e41f4ccbdf1fa4d599ae2452f3d4a1c84
mhaley-jsfun <mhaleyjsfun@gmail.com>
2022-12-02 04:48:04 -0700

    chore:adding client side to make api request and get respponse  and make the repo mono

---
+++ client/src/pages/Standing.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Standing() {
+  return (
+    <div>
+      Standing
+      <Link to="/">Go Back</Link>
+    </div>
+  );
+}
+
+export default Standing;
